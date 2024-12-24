b8cd8f8810a28559a67fd1e72310f7ca064e39d7
boris <boris@LAPTOP-655K9220>
2024-12-24 17:10:00 +0200

    Решение задания 5 №19237 с сайта КомпЕГЭ

--- ЕГКР-21-12-24/Task-5-19237.py
+++ ЕГКР-21-12-24/Task-5-19237.py
@@ -1,4 +1,20 @@
-print('Hello')
+def convert(num, sys):
+    res = ''
+    while num:
+        res += str(num % sys)
+        num //= sys
+    return res[::-1]
 
 
-print('World')
+res = []
+for N in range(1, 100_000):
+    R = convert(N, 3)
+    if N % 3 == 0:
+        R += R[-2:]
+    else:
+        R += convert(sum(map(int, R)), 3)
+    R = int(R, 3)
+    if R % 2 == 0 and R > 220:
+        res.append(R)
+
+print(min(res))
